4443ed2d94072be69ea6a4ac13e2494e0e7a2e98
Rachel Duncan <rmduncan@ucsd.edu>
2019-02-10 12:23:09 -0800

    Add new subjects and topics and it will display

--- routes/submit.js
+++ routes/submit.js
@@ -6,6 +6,13 @@ exports.submit = function(req, res) {
 	var name = req.query.name;
 	var topic1 = req.query.topic1;
 	var time1 = req.query.time1;
+	var topic2 = req.query.topic2;
+	var time2 = req.query.time2;
+	var topic3 = req.query.topic3;
+	var time3 = req.query.time3;
+	var topic4 = req.query.topic4;
+	var time4 = req.query.time4;
+
 
 	var newSubject = {
 		"name": name,
@@ -14,9 +21,33 @@ exports.submit = function(req, res) {
 				"topic": topic1,
 				"time": time1,
 				"timeLeft": time1
-			}
+			}		
 		]
 	};
+
+	if(topic2 != undefined) {
+		
+		newSubject["topics"].push({"topic":topic2, "time": time2, "timeLeft":time2});
+		
+	}
+	if(topic2 != undefined) {
+		
+		newSubject["topics"].push({"topic":topic2, "time": time2, "timeLeft":time2});
+		
+	}
+	if(topic3 != undefined) {
+		
+		newSubject["topics"].push({"topic":topic3, "time": time3, "timeLeft":time3});
+		
+	}
+	if(topic4 != undefined) {
+		
+		newSubject["topics"].push({"topic":topic4, "time": time4, "timeLeft":time4});
+		
+	}
+
+
+	console.log(newSubject);
 
 	data.subjects.push(newSubject);
 	res.render('index', data);
