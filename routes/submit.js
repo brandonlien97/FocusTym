d783a16f02e19608990b3733812739d7aecc0707
Rachel Duncan <rmduncan@ucsd.edu>
2019-02-15 15:22:51 -0800

    No blanks can be inputed on creation of a group

--- routes/submit.js
+++ routes/submit.js
@@ -8,43 +8,42 @@ exports.submit = function(req, res) {
 	var time1 = req.query.time1;
 	var topic2 = req.query.topic2;
 	var time2 = req.query.time2;
-	var topic3 = req.query.topic3;
-	var time3 = req.query.time3;
-	var topic4 = req.query.topic4;
-	var time4 = req.query.time4;
+	
+	if(name == ''){
+		res.render('add', data);
+	}
 
+    else if ((topic2 == '' && time2 != '') || (topic2 != '' && time2 == '')) {
+		res.render('add', data);
+	}
 
-	var newSubject = {
-		"name": name,
-		"topics": [
-			{
-				"topic": topic1,
-				"time": time1,
-				"timeLeft": time1
-			}		
-		]
-	};
-
-	if(topic2 != undefined || topic2 == "") {
-		
-		newSubject["topics"].push({"topic":topic2, "time": time2, "timeLeft":time2});
-		
+	// Missing parameters
+	else if((topic1 == '' && time1 != '') || (topic1 != '' && time1 == '')) {
+		res.render('add', data);
 	}
 	
-	if(topic3 != undefined|| topic3 == "") {
+	else if(topic1 != '' &&  time1 != '' ) {
+    	var newSubject = {
+			"name": name,
+		    "topics": [
+		    {
+		    	"topic": topic1,
+			    "time": time1,
+			    "timeLeft": time1
+		    }
+		    ]
+		};
+		if((topic2 != '' &&  time2 != '')) {
 		
-		newSubject["topics"].push({"topic":topic3, "time": time3, "timeLeft":time3});
-		
+			newSubject["topics"].push({"topic":topic2, "time": time2, "timeLeft":time2});
+            
+		}
+		console.log(newSubject);
+		data.groups.push(newSubject);
+		res.render('index', data);
 	}
-	if(topic4 != undefined|| topic4 == "") {
-		
-		newSubject["topics"].push({"topic":topic4, "time": time4, "timeLeft":time4});
-		
+	else {
+		res.render('add', data);
 	}
 
-
-	console.log(newSubject);
-
-	data.groups.push(newSubject);
-	res.render('index', data);
 }
