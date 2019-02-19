169254c1ee5853dc1b52318a9b5f10277872f5d3
Walker Carlson <wcarlson@ucsd.edu>
2019-02-19 13:29:24 -0800

    make new user

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 
 var data = require('../data.json');
+var users = require('../users.json');
 /*
  * GET home page.
  */
@@ -12,5 +13,18 @@ exports.login = function(req, res){
 	var u = req.query.username;
 	var p = req.query.password;
 	console.log(u, p);
+	var groups = addUser(u);
 	res.render('index', data);
+}
+
+function addUser(name){
+	if(users.name != undefined){
+		return users.groups;
+	}else{
+		var newUser = "name"{
+			groups:[]
+		};
+		users.push(newUser);
+		console.log(users);
+	}
 }
