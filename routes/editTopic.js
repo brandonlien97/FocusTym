756ba0bc548dedc9b2de62d43d1773c45c2a93c3
Rachel Duncan <rmduncan@ucsd.edu>
2019-02-19 20:35:26 -0800

    Minor fix for editing

--- routes/editTopic.js
+++ routes/editTopic.js
@@ -7,24 +7,35 @@ exports.editTopic = function(req, res) { 
   var oldTopic = req.params.topic;
   var topic = req.query.topic;
   var time = req.query.time;
-  console.log(topic);
-  console.log(time);
-  var i, j, h;
-    for(i = 0; i < data.groups.length; i++){
-      for(j = 0; j < data.groups[i].topics.length; j++){
-        if(data.groups[i].topics[j].topic == oldTopic){
-          h = i;
-          
-          var newTopic = {
-            "topic": topic,
-            "time": time,
-            "timeLeft": time
-          };
-          data.groups[i].topics[j] = newTopic;
-          break;
-        }   
-      }
+
+  var i, j, h, k;
+
+
+
+  for(i = 0; i < data.groups.length; i++){
+    for(j = 0; j < data.groups[i].topics.length; j++){
+      if(data.groups[i].topics[j].topic == oldTopic){
+        h = i;
+        k = j;  
+        break;
+      }   
     }
+  }
   
-  res.render('topics', data.groups[h]);
+  if(topic == '' || time == '') {
+    res.render('editTopics', data.groups[h].topics[k]);
+  }
+  else {
+    console.log(topic);
+    console.log(time);
+
+    var newTopic = {
+      "topic": topic,
+      "time": time,
+      "timeLeft": time
+    };
+    
+    data.groups[h].topics[k] = newTopic;  
+    res.render('topics', data.groups[h]); 
+  }
 };
