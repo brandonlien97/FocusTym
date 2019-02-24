60c8e4d43023faee7d71b305995c201ce80a0fef
Walker Carlson <wcarlson@ucsd.edu>
2019-02-23 17:51:59 -0800

    added a volume slider

--- public/js/focusTym.js
+++ public/js/focusTym.js
@@ -13,7 +13,6 @@ function goBack(){
 /*
  * Function that is called when the document is ready.
  */
-localStorage.setItem("volume",1);
 var counter = 0;
 var timeleft;
 function initializePage() {
@@ -84,8 +83,10 @@ https://www.freespecialeffects.co.uk/soundfx/animals/frog1.wav
 //var audio = new Audio("https://www.freespecialeffects.co.uk/soundfx/music/drum_01.wav");
 audio.loop = true;
 function playAudio() {
-	console.log(audio.volume);
-	audio.volume = localStorage.getItem("volume");
+	console.log(audio.volume, localStorage.getItem("volume"));
+	if(localStorage.getItem("volume") < 1){
+		audio.volume = localStorage.getItem("volume");
+	}
 	console.log(audio.volume);
 	audio.play();
 }
