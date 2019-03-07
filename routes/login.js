9b30357c94a9b75085e19df3d035be8c58bdaae5
Walker Carlson <wcarlson@ucsd.edu>
2019-03-07 13:09:20 -0800

    fixxed possible login error

--- routes/login.js
+++ routes/login.js
@@ -38,7 +38,10 @@ function addUser(usr,j){
 	var i,j;
 	for(i = 0; i < users.users.length; i++){
 		console.log(users);
-		if(!users.users[i].name.localeCompare( usr)) {
+		if(!users.users[i].name){
+			users.users[i].name = "k";
+		}
+		if(!users.users[i].name.localeCompare(usr)) {
 
 		console.log(users.users[i],usr);
 			j=i;
@@ -46,6 +49,9 @@ function addUser(usr,j){
 		}else{
 			j=0;
 		}
+	}
+	if(!users.users[j].name){
+		users.users[j].name = "k";
 	}
 	console.log(users.users[j].name.localeCompare( usr),usr,users.users[j]);
 	if(!users.users[j].name.localeCompare( usr)){
