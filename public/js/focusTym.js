572b36adb9b4c1d2a09421ba0e57195dee4b4195
Rachel Duncan <rmduncan@ucsd.edu>
2019-03-06 23:07:23 -0800

    Alarm options stay same in settings

--- public/js/focusTym.js
+++ public/js/focusTym.js
@@ -81,7 +81,7 @@ function initializePage() {
 $('#vol').slider({
 	formatter: function(value) {
 		localStorage.setItem("volume", value/10);
-		//document.getElementById("volume").value = localStorage.getItem("volume");
+		
 	}
 });
 
